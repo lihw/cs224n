07c96b0b96dfbdb8b556a2f7b86f5f3536c0fe39
lihw <lihongwei@modeloapp.com>
2018-09-13 12:00:07 +0800

    work on assignment 2

--- assignment2/assignment2/q2_initialization.py
+++ assignment2/assignment2/q2_initialization.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import math
 
 
 def xavier_weight_init():
@@ -25,6 +26,9 @@ def xavier_weight_init():
             out: tf.Tensor of specified shape sampled from the Xavier distribution.
         """
         ### YOUR CODE HERE
+        stddev =  math.sqrt(6 / np.sum(shape))
+        out = tf.Variable(tf.random_uniform(shape, dtype = tf.float32))
+        out = tf.subtract(tf.multiply(out, stddev * 2), stddev)
         ### END YOUR CODE
         return out
     # Returns defined initializer function.
@@ -34,7 +38,7 @@ def xavier_weight_init():
 def test_initialization_basic():
     """Some simple tests for the initialization.
     """
-    print "Running basic tests..."
+    print("Running basic tests...")
     xavier_initializer = xavier_weight_init()
     shape = (1,)
     xavier_mat = xavier_initializer(shape)
@@ -43,7 +47,7 @@ def test_initialization_basic():
     shape = (1, 2, 3)
     xavier_mat = xavier_initializer(shape)
     assert xavier_mat.get_shape() == shape
-    print "Basic (non-exhaustive) Xavier initialization tests pass"
+    print("Basic (non-exhaustive) Xavier initialization tests pass")
 
 
 if __name__ == "__main__":
